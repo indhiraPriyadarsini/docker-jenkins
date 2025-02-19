922ff56c5c9d09420d02a383622671e775bfa8f2
indhiraPriyadarsini <iipriyadarsini26@gmail.com>
2025-02-19 10:59:04 +0530

    implemented test

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello, Docker!');
 });
 
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`App is listening on port ${port}`);
+// });
+
+module.exports = app;
